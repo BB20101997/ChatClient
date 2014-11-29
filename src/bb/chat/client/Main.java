eedbcb15cdfec670b3887f73cfed8cfc27fa46f5
BB20101997 <blessmann-bennet@t-online.de>
2014-11-30 00:17:21 +0100

    removed unused/not implemented client without gui

--- src/bb/chat/client/Main.java
+++ src/bb/chat/client/Main.java
@@ -13,21 +13,7 @@ class Main {
 	 * @param args just the usual tArgs to start up the Program
 	 */
 	public static void main(String[] args) throws IOException {
-		boolean gui = true;
-
-		for(String s : args) {
-			if(s.equals("nogui")) {
-				gui = false;
-			}
-		}
-
-
-		//noinspection StatementWithEmptyBody
-		if(gui) {
-			new ClientGUI();
-		} else {
-			//TODO: DO something else
-		}
+		new ClientGUI();
 	}
 
 }
